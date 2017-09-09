39abb33a7637e80bae6e51a85c176ae9ec052033
Shaohua <cell.fantasy@qq.com>
2017-09-09 20:26:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask
+app = Flask(__name__)
 
 
-def sign():
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-    s = requests.session()
-    log_data = {'loginPhone':'18209347100','loginPassword':'ssh19198918'} 
-    sgn_data = {'translatorId':'WE16104633TR'}
-    
-
-    log = s.post('http://talent.woordee.com/front/truser/login', log_data) 
-    html = s.get('http://talent.woordee.com/front/truser/userCenter')
-    page = html.content
-    #print page
-
-    s.post('http://talent.woordee.com/front/truser/sign', sgn_data)
-
-    soup = BeautifulSoup(page,"html.parser")
-    txt1 = soup.find_all('a', attrs={"class":"btn-sign"})[0].get_text()
-    txt2 = soup.find_all('p', attrs={"class":"p2"})[0].get_text() 
-    return txt1+', '+txt2
-
-sign()
-
+if __name__ == "__main__":
+    app.run()
