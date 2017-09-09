be47c8a9a8315138e35fdce08a821ae612419b6b
Shaohua <cell.fantasy@qq.com>
2017-09-09 09:38:16 +0700

    Update and rename wesign27 to wesign27.py

--- wesign27.py
+++ wesign27.py
@@ -1,3 +1,14 @@
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    application.run()
+
+
 import requests
 from bs4 import BeautifulSoup
 
