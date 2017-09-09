2720ebe70318f8e9171646550cc1a8c19e86253c
Shaohua <cell.fantasy@qq.com>
2017-09-09 20:11:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ from bs4 import BeautifulSoup
 def sign():
 
     s = requests.session()
-    log_data = {'loginPhone':'18209347100','loginPassword':'ssh19198918'} #登录post数据
-    sgn_data = {'translatorId':'WE16104633TR'} #签到post数据
+    log_data = {'loginPhone':'18209347100','loginPassword':'ssh19198918'} 
+    sgn_data = {'translatorId':'WE16104633TR'}
     
 
-    log = s.post('http://talent.woordee.com/front/truser/login', log_data) #post登录地址 
-    html = s.get('http://talent.woordee.com/front/truser/userCenter') #get登陆后的地址
+    log = s.post('http://talent.woordee.com/front/truser/login', log_data) 
+    html = s.get('http://talent.woordee.com/front/truser/userCenter')
     page = html.content
     #print page
 
     s.post('http://talent.woordee.com/front/truser/sign', sgn_data)
 
     soup = BeautifulSoup(page,"html.parser")
-    txt1 = soup.find_all('a', attrs={"class":"btn-sign"})[0].get_text() #提取"已签到"文本
-    txt2 = soup.find_all('p', attrs={"class":"p2"})[0].get_text() #提取"连续签到n天"文本
+    txt1 = soup.find_all('a', attrs={"class":"btn-sign"})[0].get_text()
+    txt2 = soup.find_all('p', attrs={"class":"p2"})[0].get_text() 
     return txt1+', '+txt2
 
 if __name__ == "__main__":
