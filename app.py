37a026130f0e2cd81dd990d9465bed1fe0cec2c5
Shaohua <cell.fantasy@qq.com>
2017-09-09 20:30:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
 
+from flaskapp import app as application
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run()
