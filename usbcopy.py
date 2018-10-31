7937695d785bcff18a158fbd628c0623228290a5
Shaohua <cell.fantasy@qq.com>
2018-10-31 11:53:02 +0700

    Update usbcopy.py

--- usbcopy.py
+++ usbcopy.py
@@ -3,10 +3,11 @@ import time
 import os
 import shutil
 
-usb_path = "G:"
+usb_path = "I:"
 print "ready"
 while not os.path.exists(usb_path):
 	time.sleep(3)
 print "now"
+os.system("FileList.bat")
 shutil.copytree(usb_path, 'C:/usbcopy')
 print "done"
