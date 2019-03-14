1b5da62c9a5ef60a873e84d9827a97b01bdcd8c6
Shaohua <cell.fantasy@qq.com>
2019-03-14 16:47:01 +0700

    Update proxyGet.py

--- proxyGet.py
+++ proxyGet.py
@@ -77,7 +77,7 @@ def parse_ssr(base64_encode_str):
 
    print('server: %s, port: %s, 协议: %s, 加密方法: %s, 密码: %s, 混淆: %s, 混淆参数: %s, 协议参数: %s, 备注: %s, 分组: %s\n'
          % (server, port, protocol, method, password, obfs, obfsparam, protoparam, remarks, group))
-   f.write('server: %s, port: %s, 协议: %s, 加密方法: %s, 密码: %s, 混淆: %s, 混淆参数: %s, 协议参数: %s, 备注: %s, 分组: %s\n'
+   f.write('服务器地址: %s, 端口: %s, 协议: %s, 加密方法: %s, 密码: %s, 混淆: %s, 混淆参数: %s, 协议参数: %s, 备注: %s, 分组: %s\n'
          % (server, port, protocol, method, password, obfs, obfsparam, protoparam, remarks, group))
 
 
