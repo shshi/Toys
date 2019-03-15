71e6673968eafca7c2c23838a2a2674e79a21499
Shaohua <cell.fantasy@qq.com>
2019-03-16 00:32:06 +0700

    Update proxyGet_web.py

--- proxyGet_web.py
+++ proxyGet_web.py
@@ -88,7 +88,7 @@ def getList():
 	</table>
 </body>
 <p style="font-size:14px">------<br>* 使用说明：在https://github.com/shadowsocks/shadowsocks-windows/releases下载Shadowsocks的最新版zip文件，解压后打开Shadowsocks，按照上表对应输入服务器、端口和密码，核对加密方法后确定。在电脑下方托盘找到小飞机图标右击-->点击“启用系统代理”，“系统代理模式”-->“全局模式”。如果小飞机变为蓝色说明翻墙成功，否则重新换一行服务器信息。<br>* 建议添加多个服务器信息，方便用网不畅时切换服务器。右击小飞机图标可切换。</p>
-<div align="center" ><a href = "https://wx2.sinaimg.cn/mw690/4d20f2cfgy1g13xal3sqej20a70fajt5.jpg" style=" color:#c6a300; font-size:30px;">支付宝点我</a><a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href = "https://wx2.sinaimg.cn/mw690/4d20f2cfgy1g13yiczug9j20n80n9mz8.jpg" style=" color:#c6a300; font-size:30px;">微信点我</a></div>
+<div align="center" ><a href = "https://wx2.sinaimg.cn/mw690/4d20f2cfgy1g13xal3sqej20a70fajt5.jpg" style=" color:#c6a300; font-size:30px;">支付宝点我</a><a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href = "https://wx2.sinaimg.cn/mw690/4d20f2cfgy1g13yiczug9j20n80n9mz8.jpg" style=" color:#c6a300; font-size:30px;">微信点我</a></div>
 <div align="center"><img src="https://wx2.sinaimg.cn/mw690/4d20f2cfgy1g13zhwi6q9j20j60j6dhy.jpg" width="20%"><img src="https://wx2.sinaimg.cn/mw690/4d20f2cfgy1g13yiczug9j20n80n9mz8.jpg" width="20%"></div>
 <html>'''
     for i in lst:
